c312462386fae3941fe622c6dfe1238b23c45338
BoyanaBoyana <gtownrepresent@gmail.com>
2017-09-30 15:31:40 +0300

    New name

---
+++ Alcohol.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static java.awt.Event.F1;
+import static java.awt.Event.F2;
+
+/**
+ * Created by Acer-E1-571G on 19.7.2017 г..
+ */
+public class Alcohol {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+
+
+    }
+
+}
